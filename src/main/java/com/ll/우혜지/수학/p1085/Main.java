e73b280c67aa67571b05d162169c4d197f8fc59a
깃wooqqq <sge0225@naver.com>
2023-03-19 04:14:04 +0900

    [수학] 1085 직사각형에서 탈출

---
+++ src/main/java/com/ll/우혜지/수학/p1085/Main.java
@@ -0,0 +1,19 @@
+package com.ll.우혜지.수학.p1085;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        int w = sc.nextInt();
+        int h = sc.nextInt();
+
+        int minWidth = Math.min(x, w-x);
+        int minHeight = Math.min(y, h-y);
+
+        System.out.println(Math.min(minWidth, minHeight));
+    }
+}
