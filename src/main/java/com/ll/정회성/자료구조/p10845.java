99eb538e8509699fd7cf863ee1cb5538d616f049
HoeSeong123 <ghltjd3@gmail.com>
2023-03-26 20:18:45 +0900

    [자료구조] 10845 큐

---
+++ src/main/java/com/ll/정회성/자료구조/p10845.java
@@ -0,0 +1,52 @@
+package com.ll.정회성.자료구조;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class p10845 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Queue<Integer> queue = new LinkedList<>();
+
+        int N = Integer.parseInt(br.readLine());
+        int last = 0;
+        for (int i = 0; i < N; i++) {
+            String command = br.readLine();
+
+            if (command.startsWith("push")) {
+                int num = Integer.parseInt(command.split(" ")[1]);
+                queue.add(num);
+                last = num;
+            } else if (command.equals("pop")) {
+                if (queue.isEmpty()) {
+                    System.out.println(-1);
+                } else {
+                    System.out.println(queue.poll());
+                }
+            } else if (command.equals("size")) {
+                System.out.println(queue.size());
+            } else if (command.equals("empty")) {
+                if (queue.isEmpty()) {
+                    System.out.println(1);
+                } else {
+                    System.out.println(0);
+                }
+            } else if (command.equals("front")) {
+                if (queue.isEmpty()) {
+                    System.out.println(-1);
+                } else {
+                    System.out.println(queue.peek());
+                }
+            } else if (command.equals("back")) {
+                if (queue.isEmpty()) {
+                    System.out.println(-1);
+                } else {
+                    System.out.println(last);
+                }
+            }
+        }
+    }
+}
--- src/main/java/com/ll/정회성/자료구조/p2504.java
+++ src/main/java/com/ll/정회성/자료구조/p2504.java
@@ -3,8 +3,6 @@ package com.ll.정회성.자료구조;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Stack;
 
 public class p2504 {
@@ -23,28 +21,35 @@ public class p2504 {
             if(bracket.equals("(")) {
                 stack.push(bracket);
                 tmp *= 2;
-            } else if(bracket.equals(")")) {
+            }
+            else if(bracket.equals(")")) {
                 if(stack.isEmpty() || !stack.peek().equals("(")) {
                     System.out.println(0);
                     return;
                 }
-                stack.pop();
+
                 if(brackets[i-1].equals("(")) {
                     answer += tmp;
                 }
+
+                stack.pop();
                 tmp /= 2;
-            } else if(bracket.equals("[")) {
+            }
+            else if(bracket.equals("[")) {
                 stack.push(bracket);
                 tmp *= 3;
-            } else if(bracket.equals("]")) {
+            }
+            else if(bracket.equals("]")) {
                 if(stack.isEmpty() || !stack.peek().equals("[")) {
                     System.out.println(0);
                     return;
                 }
-                stack.pop();
+
                 if(brackets[i-1].equals("[")) {
                     answer += tmp;
                 }
+
+                stack.pop();
                 tmp /= 3;
             } else {
                 System.out.println(0);
