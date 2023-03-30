bbcfa532a3294d9a21d8f9ff8ae2e2b851124e5d
깃wooqqq <sge0225@naver.com>
2023-03-31 02:11:14 +0900

    [재귀정렬] 11729 하노이 탑 이동 순서

--- src/main/java/com/ll/우혜지/재귀정렬/p11729/Main.java
+++ src/main/java/com/ll/우혜지/재귀정렬/p11729/Main.java
@@ -1,15 +1,28 @@
 package com.ll.우혜지.재귀정렬.p11729;
 
-import java.util.Scanner;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
+    public static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        int N = Integer.parseInt(br.readLine());
 
+        sb.append((int) (Math.pow(2, N) - 1)).append('\n');
+
+        Hanoi(N, 1, 2, 3);
+        System.out.println(sb);
     }
-    public static int Hanoi(int N) {
-        return 1;
+
+    public static void Hanoi(int N, int start, int sub, int goal) {
+        if (N == 1) {
+            sb.append(start + " " + goal + "\n");
+            return;
+        }
+
+        Hanoi(N - 1, start, goal, sub);
+        sb.append(start + " " + goal + "\n");
+        Hanoi(N - 1, sub, start, goal);
     }
 }
